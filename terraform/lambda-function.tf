2aa8bb0878c1e3296cb7ed2b4a7ad0792d789246
Francisco Ramón Sánchez Favela <frsanchezf@gbm.com.mx>
2018-04-25 09:24:17 -0500

    Fix travis definition

--- terraform/lambda-function.tf
+++ terraform/lambda-function.tf
@@ -2,12 +2,12 @@ resource "aws_lambda_function" "hello-travis-lambda" {
   function_name    = "hello-travis"
   description      = "Lambda function created to test travis ci/cd process"
   runtime          = "go1.x"
-  filename         = "${var.lambda-zip}"
+  filename         = "${var.lambda_zip}"
   role             = "${aws_iam_role.lambda-role.arn}"
-  source_code_hash = "${base64sha256(file("${var.lambda-zip}"))}"
-  handler          = "${var.lambda-handler}"
-  timeout          = "${var.lambda-timeout}"
-  memory_size      = "${var.lambda-memory}"
+  source_code_hash = "${base64sha256(file("${var.lambda_zip}"))}"
+  handler          = "${var.lambda_handler}"
+  timeout          = "${var.lambda_timeout}"
+  memory_size      = "${var.lambda_memory}"
   kms_key_arn      = "${aws_kms_key.lambda-encryption-key.arn}"
 
   tags {
